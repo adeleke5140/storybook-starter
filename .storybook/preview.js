8a5de5e8875b26b6f5d2a61eb108ac1133def506
Kenny <adelekekehinde06@gmail.com>
2023-04-01 04:47:15 +0100

    feat: update preview with relevant functions

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,18 @@
-import "../src/index.css"
+import "../src/index.css";
+
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+initialize();
+
+//I see a decorator as an external source of data we use in our stories but that might not be entirely accurate
+export const decorators = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/
-    }
-  }
-}
+      date: /Date$/,
+    },
+  },
+};
