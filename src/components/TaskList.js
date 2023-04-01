1f033dcb6970c2df455c8e2defb64e8e819a523c
Kenny <adelekekehinde06@gmail.com>
2023-04-01 03:16:17 +0100

    feat: add propTypes and update styling

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,12 @@
-import React from "react"
-import Task from "./Task"
+import React from "react";
+import Task from "./Task";
+import PropTypes from "prop-types";
 
 export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   const events = {
     onPinTask,
-    onArchiveTask
-  }
+    onArchiveTask,
+  };
 
   const LoadingRow = (
     <div className="loading-item">
@@ -14,20 +15,53 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
         <span>Loading</span> <span>cool</span> <span>state</span>
       </span>
     </div>
-  )
+  );
   if (loading) {
-    return <div className="list-items">loading</div>
+    return (
+      <div className="list-items" data-testid="loading" key={"loading"}>
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
   }
 
   if (tasks.length === 0) {
-    return <div className="list-items">empty</div>
+    return (
+      <div className="list-items">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <p className="title-message">You have no tasks</p>
+          <p className="subtitle-message">You have no tasks</p>
+        </div>
+      </div>
+    );
   }
+
+  const tasksInOrder = [
+    ...tasks.filter((t) => t.state === "TASK_PINNED"),
+    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+  ];
 
   return (
     <div className="list-items">
-      {tasks.map((task) => (
+      {tasksInOrder.map((task) => (
         <Task key={task.id} task={task} {...events} />
       ))}
     </div>
-  )
+  );
 }
+
+TaskList.propTypes = {
+  loading: PropTypes.bool,
+  tasks: PropTypes.arrayOf(Task.PropTypes.task).isRequired,
+  onPinTask: PropTypes.func,
+  onArchiveTask: PropTypes.func,
+};
+
+TaskList.defaultProps = {
+  loading: false,
+};
